859389ad40c8673f7ea16a664a8f33ca0b04413d
Ankit yadav <ankityaduvansh1rao9818@gmail.com>
2026-01-28 23:32:52 +0530

    some3

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174'],
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174', 'https://blog-proj1.onrender.com'],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
